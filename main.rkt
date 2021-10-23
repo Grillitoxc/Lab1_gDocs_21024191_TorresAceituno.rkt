4be9e6b8317b82c388b23c1bf2cea2379752b096
Christopher <christopher.torres@usach.cl>
2021-10-22 23:38:33 -0300

    (WIP) Planteo / creación de función login
    Se crea login pero aún se debían crear otros parámetros para dejar más clara la implementación.

--- main.rkt
+++ main.rkt
@@ -1,13 +1,13 @@
 #lang racket
-(require "TDAParadigmaDocs.rkt")
+(require "TDAParadigmadocs.rkt")
 (require "TDAUser.rkt")
 (require "TDADate.rkt")
 
 ;descripción: Función que registra a un usuario verificando que este sea único
 ;dom: Paradigmadocs X date X string X string
-;rec: ParadigmaDocs
+;rec: Paradigmadocs
 ;recursión: Natural
-(define (register paradigmaDocs date username password)
+(define (register paradigmadocs date username password)
   (define (registrado? listaUsuarios usuario_1)
     (if (empty? listaUsuarios)
         #f
@@ -15,11 +15,45 @@
             #t
             (registrado? (cdr listaUsuarios) usuario_1)))
     )
-  (if (registrado? (getLista1 paradigmaDocs) (usuario username password date))
-      paradigmaDocs
-      (setLista1 paradigmaDocs (cons (usuario username password date)(getLista1 paradigmaDocs))))
+  (if (registrado? (getLista1 paradigmadocs) (usuario username password date))
+      paradigmadocs
+      (setLista1 paradigmadocs (cons (usuario username password date)(getLista1 paradigmadocs))))
   )
 
-(define Gdocs (paradigmaDocs "gDocs" (fecha 25 10 2021) encryptFn encryptFn))
-(define emptyGDocs (paradigmaDocs "gDocs" (fecha 25 10 2021) encryptFn encryptFn))
-(define gDocs1 (register (register (register emptyGDocs (fecha 25 10 2021) "user2" "pass1") (fecha 25 10 2021) "user2" "pass2") (fecha 25 10 2021) "user3" "pass3"))
+
+;descripción: Función que autentica a un usuario registrado y le permite ejecutar un comando específico.
+;dom: paradigmadocs X string X string X function 
+;rec: Paradigmadocs
+;recursión: 
+;(define (login paradigmadocs username pasword operation)
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;---EJEMPLOS DE CADA FUNCIÓN---
+; GENERANDO PARADIGMADOCS
+(define Gdocs000 (paradigmadocs "Gdocs" (fecha 25 10 2021) encryptFn encryptFn))
+
+; 1) REGISTER 
+(define Gdocs011 (register (register (register Gdocs000 (fecha 25 10 2021) "user2" "pass1") (fecha 25 10 2021) "user2" "pass2") (fecha 25 10 2021) "user3" "pass3"))
+(define Gdocs012 (register Gdocs000 (fecha 25 3 2020) "user" "pass"))
+(define Gdocs013 (register (register Gdocs000 (fecha 25 3 2020) "user" "pass") (fecha 25 3 2020) "user" "pass2"))
