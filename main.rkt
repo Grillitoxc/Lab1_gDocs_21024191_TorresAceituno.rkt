e31b9d7c30850d5af02a4ffadf77be705bd5539b
Christopher <christopher.torres@usach.cl>
2021-11-11 18:11:43 -0300

    Función ADD y restoreVersion
    Se termina la función ADD y se comienza restoreVersion.

--- main.rkt
+++ main.rkt
@@ -43,12 +43,14 @@
       (cond
         [(eq? operation create)(operation (setLista2 paradigmadocs (list username)))]
         [(eq? operation share)(operation (setLista2 paradigmadocs (list username)))]
+        [(eq? operation add)(operation (setLista2 paradigmadocs (list username)))]
         [(eq? operation revokeAllAccesses)(operation (setLista2 paradigmadocs (list username)))]
         [else paradigmadocs]
         )
       (cond
         [(eq? operation create)(operation paradigmadocs)]
         [(eq? operation share)(operation paradigmadocs)]
+        [(eq? operation add)(operation paradigmadocs)]
         [(eq? operation revokeAllAccesses)(operation paradigmadocs)]
         [else paradigmadocs]
         )
@@ -65,14 +67,14 @@
     (if (null? (getLista2 paradigmadocs))
         paradigmadocs
         (setLista3 (setLista2 paradigmadocs null)
-                   (documento nombre date ((getEncrypt paradigmadocs) contenido) (car(getLista2 paradigmadocs)) (length (getLista3 paradigmadocs)) null null))
+                   (documento nombre date ((getEncrypt paradigmadocs) contenido) (car(getLista2 paradigmadocs)) (length (getLista3 paradigmadocs)) null null ))
         )
     )
   )
 
 
 ;-SHARE-
-;descripción: Función que le da distintos tipos de accesos a usuarios que previamente se hayan registrado. En caso de que el usuario sea el propietario del documento, este no puede darse permisos.
+;descripción: Función que le da distintos tipos de accesos a usuarios que previamente se hayan registrado. En caso de que el usuario sea el propietario del documento, este no puede darse permisos
 ;dom: Paradigmadocs X Entero X Lista de accesos
 ;rec: Paradigmadocs
 ;recursión: de cola (buscarDoc, seleccionarDoc, buscarUserDoc)
@@ -91,14 +93,58 @@
 
 
 ;-ADD-
-;descripción: Función que elimina todos accesos de todos los documentos
+;descripción: Función que agrega un contenido al documento considerando que esto sólo lo puede hacer el propietario/autor de un documento o aquellos usuarios que tengan el permiso de escritura.
+;             Además, el documento con su contenido pasa a ser la versión actual de este, mientras que se pasa a un historial de versiones el documento anterior.
 ;dom: Paradigmadocs X Entero X Fecha X String
 ;rec: Paradigmadocs
-;(define (add paradigmadocs)
-;  (lambda(idDoc date contenidoTexto)
-;    (if (or (esPropietario? (getLista3 paradigmadocs) (car (getLista2 paradigmadocs)) idDoc)
-;            ())
+;recursión: de cola (buscarDoc, seleccionarDoc, filtrarUsuariosEscritura, 
+(define (add paradigmadocs)
+  (lambda(idDoc date contenidoTexto)
+    (if (null? (getLista2 paradigmadocs))
+        paradigmadocs
+        (if (or (esPropietario? (getLista3 paradigmadocs) (car (getLista2 paradigmadocs)) idDoc)
+                (filtrarUsuariosEscritura (getAccesos (seleccionarDoc (getLista3 paradigmadocs) idDoc)) (car (getLista2 paradigmadocs)) ))
+            (if (buscarDoc (getLista3 paradigmadocs) idDoc)
+                (setLista3_implante (setLista2 paradigmadocs null)
+                                    (reverse (list-set (reverse (getLista3 paradigmadocs)) idDoc (setHistorial (seleccionarDoc (getLista3 paradigmadocs) idDoc) (documento
+                                                                                                                                                                 (getTitulo (seleccionarDoc (getLista3 paradigmadocs) idDoc))
+                                                                                                                                                                 (getFechaD (seleccionarDoc (getLista3 paradigmadocs) idDoc))
+                                                                                                                                                                 (getContenido (seleccionarDoc (getLista3 paradigmadocs) idDoc))
+                                                                                                                                                                 (getAutor (seleccionarDoc (getLista3 paradigmadocs) idDoc))
+                                                                                                                                                                 idDoc
+                                                                                                                                                                 null
+                                                                                                                                                                 null) contenidoTexto date idDoc paradigmadocs))))
+                paradigmadocs
+                )
+            paradigmadocs
+            )
+        )
+    )
+  )
 
+
+;-RESTOREVERSION-
+;descripción:
+;dom:
+;rec:
+;recursión:
+(define (restoreVersion paradigmadocs)
+  (lambda(idDoc idVersion)
+    (if (null? (getLista2 paradigmadocs))
+        (if (esPropietario? (getLista3 paradigmadocs) (car (getLista2 paradigmadocs)) idDoc)
+            (if (buscarDoc (getLista3 paradigmadocs) idDoc)
+                #t
+                #f
+                )
+            #f
+            )
+        paradigmadocs
+        )
+    )
+  )
+
+
+    
 
 
 ;-REVOKEALLACCESSES-
@@ -123,25 +169,29 @@
 
 ; 2) LOGIN
 (define Gdocs021 (login Gdocs011 "user3" "pass3" share))
-(define Gdocs022 ((login Gdocs011 "user1" "pass1" create) (fecha 12 12 1212) "doc1" "contenido1"))
-(define Gdocs023 ((login Gdocs022 "user2" "pass2" create)(fecha 12 12 1212) "doc2" "contenido2"))
+(define Gdocs022 ((login Gdocs011 "user1" "pass1" create) (fecha 10 10 2021) "doc1" "contenido1"))
+(define Gdocs023 ((login Gdocs022 "user2" "pass2" create)(fecha 10 10 2021) "doc2" "contenido2"))
 
 ; 3) CREATE
-(define Gdocs031 ((login Gdocs011 "user3" "pass3" create) (fecha 12 12 1220) "primerTitulo" "contenido"))
-(define Gdocs032 ((login Gdocs012 "user" "pass" create) (fecha 12 12 1212) "doc1" "contenido1"))
-(define Gdocs033 ((login Gdocs032 "user" "pass" create) (fecha 12 12 1212) "doc2" "contenido2"))
+(define Gdocs031 ((login Gdocs011 "user3" "pass3" create) (fecha 10 10 2021) "primerTitulo" "contenido"))
+(define Gdocs032 ((login Gdocs012 "user" "pass" create) (fecha 10 10 2021) "doc1" "contenido1"))
+(define Gdocs033 ((login Gdocs032 "user" "pass" create) (fecha 10 10 2021) "doc2" "contenido2"))
 
-; 3) SHARE
-(define Gdocs041 ((login Gdocs023 "user1" "pass1" share) 0 (access "user3" #\r) (access "user2" #\c)))
+; 4) SHARE
+(define Gdocs041 ((login Gdocs023 "user1" "pass1" share) 0 (access "user3" #\r) (access "user2" #\w) (access "user2" #\c)))
 (define Gdocs042 ((login Gdocs023 "user1" "pass1" share) 0 (access "user1" #\r) (access "user2" #\c)))
 (define Gdocs043 ((login Gdocs023 "user1" "pass1" share) 1 (access "user3" #\r) (access "user3" #\w) (access "user2" #\c)))
 
-; 4) 
-(define lol1 (sacarUsuariosAccesos (list (access "user2" #\r) (access "user1" #\r))))
-(define lol2 (sacarUsuariosRegistrados (getLista1 Gdocs011)))
-(define lol3 (filtrarAccesos Gdocs011 (list (access "user2" #\r) (access "user1" #\r)) "user5"))
 
-; 5) REVOKEALLACCESSES
-(define Gdocs051 (login Gdocs041 "user1" "pass1" revokeAllAccesses))
-(define Gdocs052 (login Gdocs042 "user1" "pass1" revokeAllAccesses))
-(define Gdocs053 (login Gdocs043 "user0" "pass1" revokeAllAccesses))
+; 5) ADD
+(define Gdocs051 ((login Gdocs041 "user2" "pass2" add) 0 (fecha 11 11 2021) " Contenido extra"))
+(define Gdocs052 ((login Gdocs051 "user2" "pass2" add) 0 (fecha 12 11 2021) " Más contenido"))
+(define Gdocs053 ((login Gdocs041 "user3" "pass3" add) 0 (fecha 11 11 2021) " Contenido extra"))
+
+; 6) RESTOREVERSION
+
+
+; 7) REVOKEALLACCESSES
+(define Gdocs071 (login Gdocs051 "user1" "pass1" revokeAllAccesses))
+(define Gdocs072 (login Gdocs042 "user1" "pass1" revokeAllAccesses))
+(define Gdocs073 (login Gdocs043 "user0" "pass1" revokeAllAccesses))
